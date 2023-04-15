8f5be130ab8eeff81e09c6a66ee7afd37d39e4ef
0x13f <22893194+mr0x13f@users.noreply.github.com>
2023-04-15 12:48:55 +0200

    Create gmod-workshop-lua-generator.js

---
+++ gmod-workshop-lua-generator.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+const https = require('https');
+
+function now() {
+    let date = new Date();
+    return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}`;
+}
+
+async function httpGet(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, response => {
+            let data = [];
+
+            response.on('data', chunk => {
+                data.push(chunk);
+            });
+
+            response.on('end', () => {
+                if (response.statusCode == 200)
+                    resolve(Buffer.concat(data).toString());
+                else
+                    reject(err);
+            })
+        }).on('error', err => {
+            reject(err);
+        });
+    });
+}
+
+function consumeUntil(text, search) {
+    let startIndex = text.indexOf(search);
+    let endIndex = startIndex + search.length;
+    let resultingText = text.substring(endIndex);
+    let consumedText = text.substring(0, startIndex);
+    return [resultingText, consumedText];
+}
+
+async function readCollection(collectionId) {
+    let html = await httpGet(`https://steamcommunity.com/workshop/filedetails/?id=${collectionId}`);
+    let lua = '';
+
+    [html, _] = consumeUntil(html, '<div class="workshopItemTitle">');
+    [html, collectionName] = consumeUntil(html, '</div>');
+    [html, _] = consumeUntil(html, '<span class="childCount">(');
+    [html, itemsCount] = consumeUntil(html, ')</span>');
+
+    lua += '\n----------------------------------------------------------------------';
+    lua += `\n-- ${collectionName}`;
+    lua += `\n-- # of items: ${itemsCount}`;
+    lua += `\n-- https://steamcommunity.com/workshop/filedetails/?id=${collectionId}`;
+    lua += '\n----------------------------------------------------------------------';
+    lua += '\n';
+
+    while(html.indexOf('<div class="collectionItemDetails">') > -1) {
+        [html, _] = consumeUntil(html, '<div class="collectionItemDetails">');
+        [html, _] = consumeUntil(html, 'href="https://steamcommunity.com/sharedfiles/filedetails/?id=');
+        [html, fileId] = consumeUntil(html, '"');
+        [html, _] = consumeUntil(html, '<div class="workshopItemTitle">');
+        [html, fileName] = consumeUntil(html, '</div>');
+
+        lua += `\nresource.AddWorkshop("${fileId}") -- ${fileName}`;
+    }
+
+    lua += '\n';
+    lua += '\n';
+    lua += '\n';
+
+    [html, _] = consumeUntil(html, 'class="collectionChildren">');
+
+    while(html.indexOf('class="workshopItem">') > -1) {
+        [html, _] = consumeUntil(html, 'class="workshopItem">');
+        [html, _] = consumeUntil(html, 'href="https://steamcommunity.com/sharedfiles/filedetails/?id=');
+        [html, childId] = consumeUntil(html, '"');
+
+        lua += await readCollection(childId);
+    }
+
+    return lua;
+}
+
+async function main() {
+    let collectionId = process.argv[2];
+    let workshopFilePath = process.argv[3];
+
+    let lua = `-- Generated on ${now()}`;
+    lua += '\n';
+    lua += await readCollection(collectionId);
+
+    fs.writeFileSync(workshopFilePath, lua, {flag: 'w', encoding: 'utf8'});
+}
+
+main();
